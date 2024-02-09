7adbfcf706d7e1cd1916d6a9d58bdf94e9c76811
Atif-1 <atif1712001@gmail.com>
2024-02-10 02:43:40 +0530

    shop app with sequelize associations and its magic methods

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+
 const sequelize=require('./util/database');
+const Product=require('./models/product');
+const User=require('./models/user');
 
 const app = express();
 
@@ -17,13 +20,33 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next)=>{
+	User.findAll({where:{id:1}}).then(user=>{
+		req.user=user[0];
+		next();
+	})
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+//associating product with user
+Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'});
+User.hasMany(Product);
 
-sequelize.sync().then(() => {
-	app.listen(8000)
+sequelize.sync().then(res => {
+	return User.findAll({where:{id:1}});
+})
+.then(user=>{
+	if(!user){
+		return User.create({name:'Max',email:'test@test.com'})
+	}
+	return user;
+})
+.then(user=>{
+	// console.log(user);
+	app.listen(8000);
 }).catch((err) => {
 	console.log(err);
 });
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,11 +13,11 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title:title,
     price:price,
     imageUrl:imageUrl,
-    description:description
+    description:description,
   }).then(()=>{
     res.redirect('/products');
   }).catch(err=>console.log(err))
@@ -29,16 +29,18 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId=req.params.productId;
-  Product.findAll({where:{id:prodId}}).then(([user])=>{
-    console.log(user);
-    if(!user){
+  req.user.getProducts({where:{id:prodId}})
+  // Product.findAll({where:{id:prodId}})
+  .then((products)=>{
+    const product=products[0];
+    if(!product){
       return res.redirect('/');
     }
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing:editMode,
-      product:user
+      product:product
     });
   }).catch(err=>console.log(err))
 };
@@ -59,9 +61,9 @@ exports.postEditProduct=(req,res,next)=>{
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then((user)=>{
+  req.user.getProducts().then((products)=>{
     res.render('admin/products', {
-      prods: user,
+      prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
